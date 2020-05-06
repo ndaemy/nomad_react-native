963f4ff7961ce0ac273ae88e1de622de69b1c29c
yuyaebean <yuyaebean@gmail.com>
2020-05-06 20:19:25 +0900

    1.2 cacheFonts

--- App.tsx
+++ App.tsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 import { Text, Image } from 'react-native'
 import { AppLoading } from 'expo'
 import { Asset } from 'expo-asset'
+import * as Font from 'expo-font'
+import { Ionicons } from '@expo/vector-icons'
 
-const cacheImages = (images: Array<any>): Array<Promise<any>> =>
+const cacheImages = (images: any[]) =>
   images.map(image => {
     if (typeof image === 'string') {
       return Image.prefetch(image)
@@ -12,25 +14,32 @@ const cacheImages = (images: Array<any>): Array<Promise<any>> =>
     }
   })
 
+const cacheFonts = (fonts: any[]) => fonts.map(font => Font.loadAsync(font))
+
 export default function App() {
   const [isReady, setIsReady] = useState<boolean>(false)
-  async function loadAssets() {
+
+  function loadAssets(): Promise<any> {
     const images = cacheImages([
       'https://ww.namu.la/s/c29ae1e63fd77beb9fc5a86ba6bab190a1def549276a50780042da82008ea4287244d2ae47a533509a43da846139a7f28bb656ac7c045f7373641afcdd8692c08518b61d7cae55560f663ce364315a34e8fd273a7edfa6d3ed7a7be349f8a718',
       require('./assets/splash.png'),
     ])
-    console.log(images)
+    const fonts = cacheFonts([Ionicons.font])
+
+    return Promise.all([...images, ...fonts])
   }
+
   function onFinish() {
     setIsReady(true)
   }
+
   return isReady ? (
     <Text>Ready!</Text>
   ) : (
     <AppLoading
       startAsync={loadAssets}
-      onError={console.error}
       onFinish={onFinish}
+      onError={console.error}
     />
   )
 }
