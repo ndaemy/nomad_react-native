d50fa57a89f85319adca34a1ba89e40e2259139d
yuyaebean <yuyaebean@gmail.com>
2020-05-20 18:28:15 +0900

    3.3 Slider content

--- components/Movies/Slide.tsx
+++ components/Movies/Slide.tsx
@@ -1,19 +1,41 @@
 import React from 'react'
-import { Dimensions, Image } from 'react-native'
 import styled from 'styled-components/native'
 import { apiImage } from '../../api'
 
-const { width: WIDTH, height: HEIGHT } = Dimensions.get('screen')
-
 const Container = styled.View`
-  width: ${WIDTH}px;
-  height: ${HEIGHT / 4}px;
-  background-color: red;
+  width: 100%;
+  height: 100%;
 `
 
 const BG = styled.Image`
   width: 100%;
   height: 100%;
+  opacity: 0.6;
+  position: absolute;
+`
+
+const Content = styled.View`
+  flex-direction: row;
+`
+
+const Data = styled.View`
+  width: 50%;
+`
+
+const Title = styled.Text`
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+`
+
+const Votes = styled.Text`
+  color: white;
+  opacity: 0.7;
+`
+
+const Overview = styled.Text`
+  color: white;
+  opacity: 0.7;
 `
 
 interface Props {
@@ -30,6 +52,13 @@ export default function Slide(props: Props) {
   return (
     <Container>
       <BG source={{ uri: apiImage(backgroundImage) }} />
+      <Content>
+        <Data>
+          <Title>{title}</Title>
+          <Votes>{votes} / 10</Votes>
+          <Overview>{overview}</Overview>
+        </Data>
+      </Content>
     </Container>
   )
 }
--- screens/Movies/MoviesPresenter.tsx
+++ screens/Movies/MoviesPresenter.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { ActivityIndicator } from 'react-native'
+import { ActivityIndicator, Dimensions } from 'react-native'
 import styled from 'styled-components/native'
 import Swiper from 'react-native-web-swiper'
 import Slide from '../../components/Movies/Slide'
+
+const { width: WIDTH, height: HEIGHT } = Dimensions.get('screen')
 
 const Container = styled.View`
   flex: 1;
   background-color: black;
   justify-content: center;
+`
+
+const SliderContainer = styled.View`
+  width: ${WIDTH}px;
+  height: ${HEIGHT / 4}px;
 `
 
 interface Props {
@@ -26,20 +33,20 @@ export default function MoviesPresenter(props: Props) {
       {props.loading ? (
         <ActivityIndicator color='white' size='small' />
       ) : (
-        <>
+        <SliderContainer>
           <Swiper controlsEnabled={false} loop timeout={3}>
             {props.nowPlaying.map(movie => (
               <Slide
                 key={movie.id}
                 id={movie.id}
-                title={movie.original_language}
+                title={movie.original_title}
                 overview={movie.overview}
                 votes={movie.vote_average}
                 backgroundImage={movie.backdrop_path}
               />
             ))}
           </Swiper>
-        </>
+        </SliderContainer>
       )}
     </Container>
   )
