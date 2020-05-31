1267dc191af9857cf83465f04bdb02697018cd62
yuyaebean <yuyaebean@gmail.com>
2020-05-31 14:39:28 +0900

    5.1 Search Logic

--- screens/Search/SearchContainer.tsx
+++ screens/Search/SearchContainer.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchPresenter from './SearchPresenter'
+import { movieApi, tvApi } from '../../api'
 
 interface Props {}
 
 export default function SearchContainer({}: Props) {
-  return <SearchPresenter></SearchPresenter>
+  const [keyword, setKeyword] = useState('')
+  const [results, setResults] = useState({
+    movies: [],
+    shows: [],
+    movieError: null,
+    showError: null,
+  })
+
+  const onChange = (text: string) => setKeyword(text)
+
+  const onSubmit = async () => {
+    const [movies, movieError] = await movieApi.search(keyword)
+    const [shows, showError] = await tvApi.search(keyword)
+    setResults({
+      movies,
+      shows,
+      movieError,
+      showError,
+    })
+
+    console.log(results)
+  }
+
+  return (
+    <SearchPresenter
+      keyword={keyword}
+      {...results}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  )
 }
--- screens/Search/SearchPresenter.tsx
+++ screens/Search/SearchPresenter.tsx
@@ -1,17 +1,42 @@
 import React from 'react'
 import styled from 'styled-components/native'
 import Input from '../../components/Search/Input'
+import HorizontalSlider from '../../components/HorizontalSlider'
+import Horizontal from '../../components/Horizontal'
 
 const Container = styled.ScrollView`
   background-color: black;
 `
 
-interface Props {}
+interface Props {
+  keyword: string
+  movies: Array<any>
+  shows: Array<any>
+  onChange(e: any): void
+  onSubmit(e: any): void
+}
 
-export default function SearchPresenter({}: Props) {
+export default function SearchPresenter(props: Props) {
+  const { keyword, movies, shows, onChange, onSubmit } = props
+
   return (
     <Container>
-      <Input placeholder='Write a keyword' />
+      <Input
+        placeholder='Write a keyword'
+        value={keyword}
+        onChange={onChange}
+        onSubmit={onSubmit}
+      />
+      <HorizontalSlider title={'Movie results'}>
+        {movies.map(movie => (
+          <Horizontal />
+        ))}
+      </HorizontalSlider>
+      <HorizontalSlider title={'TV results'}>
+        {shows.map(show => (
+          <Horizontal />
+        ))}
+      </HorizontalSlider>
     </Container>
   )
 }
